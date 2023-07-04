e2be4cb780e40299c32ef0a0a75dcac6e8157fd9
ecekoprucu <ecekoprucu@gmail.com>
2023-07-04 17:15:53 +0200

    alert

--- src/pages/Login/Form/index.tsx
+++ src/pages/Login/Form/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { Input } from 'src/components/input';
 import { CustomButton } from 'src/components/button';
@@ -6,15 +6,24 @@ import * as Styled from './styled';
 
 export const CustomForm = () => {
   const navigate = useNavigate();
+  const formRef = useRef<HTMLFormElement>(null);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    navigate('/dashboard');
+    const email = (formRef.current?.elements[0] as HTMLInputElement).value;
+    const password = (formRef.current?.elements[1] as HTMLInputElement).value;
+
+    if (email.length > 0 && password.length > 0) {
+      navigate('/dashboard');
+    } else {
+      alert('Please enter email and password');
+    }
   };
 
   return (
     <form
       onSubmit={handleSubmit}
+      ref={formRef}
       style={{
         width: '100%',
       }}
